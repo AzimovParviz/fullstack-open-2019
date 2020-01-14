602cb36c6aff054c474fe76d131fdfdf8fe7c0b5
Parviz Azimov <parviz.azimov@student.lamk.fi>
2020-01-14 10:18:54 +0300

    Phonebook step3

--- Phonebook/step1/src/App.js
+++ Phonebook/step1/src/App.js
@@ -3,56 +3,62 @@ import React, { useState } from 'react'
 const App = (props) => {
     const [ persons, setPersons] = useState(props.persons)
     const [ newName, setNewName ] = useState('add...')
+    const [ newNumber, setNewNumber ] = useState('type...')
     console.log(newName);
     const rows = () => persons.map(person =>
-				   <li key={person.id}>{person.name}</li>)
+				   <li key={person.id}>{person.name} {person.number}</li>)
     const addName = (event) => {
 	event.preventDefault()	
 	const nameObject = {
 	    name: newName,
+	    number: newNumber,
 	    id: persons.length+1,
 	    date: new Date().toISOString()
 	}
 	
-	/*if(persons.filter(person => (person.name === newName)))
-	    alert('${newName} already exists smh')
-	else {
-	    setPersons(persons.concat(nameObject))
-	    setNewName('')
-	}*/
 	var found = false;
 	for(var i = 0; i < persons.length; i++) {
 	    if (persons[i].name == newName) {
 		found = true;
 		alert('${newName} already exists smh')
 		break;
-	    }
+	    }	    
 	}
 	if(!found) {
 	    setPersons(persons.concat(nameObject))
 	    setNewName('')
+	    setNewNumber('')
 	}
     }
-    
+
     const handleNameChange = (event) => {
 	console.log(event.target.value)
 	setNewName(event.target.value)
     }
+    
+    const handleNumberChange = (event) => {
+	console.log(event.target.value)
+	setNewNumber(event.target.value)
+    }
+    
     console.log(newName)
     return (
-	  <div>
-	  <h2>Phonebook</h2>
-	  <form onSubmit={addName}>
-          <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-          </div>
-          <div>	  
-          <button type="submit">add</button>
-          </div>
-	  </form>
+	    <div>
+	    <h2>Phonebook</h2>
+	    <form onSubmit={addName}>
+            <div>
+            name: <input value={newName} onChange={handleNameChange}/>
+            </div>
+	    <div>
+	    number: <input value={newNumber} onChange={handleNumberChange} />
+	    </div>
+            <div>	  
+            <button type="submit">add</button>
+            </div>
+	    </form>
 	    <h2>Numbers</h2>
 	    {rows()}
-	  </div>
+	</div>
   )
 }
 
--- Phonebook/step1/src/index.js
+++ Phonebook/step1/src/index.js
@@ -5,12 +5,14 @@ import App from './App.js'
 const persons = [
   {
     id: 1,
-    name: 'Moot Mootovich',
+      name: 'Moot Mootovich',
+      number: '+7800553535',
     date: '2019-05-30T17:30:31.098Z',
   },
   {
     id: 2,
-    name: 'Tyrell Welling',
+      name: 'Tyrell Welling',
+      number: '+123456789',
     date: '2019-05-30T18:39:34.091Z',
   }
 ]
